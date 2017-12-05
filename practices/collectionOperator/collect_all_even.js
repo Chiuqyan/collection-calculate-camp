43ed3b85d688897cdf106f30da1f9c972ee1cdd6
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-05 22:53:19 +0800

    finish the first item

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    let res = [];
+    for (let number of collection) {
+        if (number % 2 == 0) {
+            res.push(number);
+
+        }
+    }
+    return res;
 }
 
 module.exports = collect_all_even;
