f914d8197a50d690f1284c02846b60b6133f9d98
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-09 00:06:45 +0800

    completed the flatten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,20 @@
 'use strict';
+var flatten = [];
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    flat(collection);
+    return flatten;
+}
+
+function flat(collection) {
+    collection.filter(item => {
+        if (Array.isArray(item))
+            flat(item);
+        else
+            flatten.push(item);
+    });
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,27 @@
 'use strict';
+var flatten = [];
+
+function flat(collection) {
+    let res = [];
+    collection.filter(item => {
+        if (Array.isArray(item))
+            flat(item);
+        else
+            flatten.push(item);
+    });
+}
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    let res = [];
+    flat(collection);
+    flatten.filter(item => {
+        if (res.indexOf(item) === -1)
+            res.push(item);
+    });
+    return res;
+
 }
 
 module.exports = double_to_one;
