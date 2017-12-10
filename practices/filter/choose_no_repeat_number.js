d2ebeb29865d72e0e15163eec729de5ae9c5b7d7
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-11 01:13:26 +0800

    change

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -4,9 +4,8 @@ function choose_no_repeat_number(collection) {
 
     //在这里写入代码
     let res = [];
-    collection.filter(item => {
-        if (res.indexOf(item) === -1)
-            res.push(item);
+    res = collection.filter(item => {
+        res.indexOf(item) === -1
     })
     return res;
 }
